3f7550f5a280c901ce772bc47296689af57cf75f
paulkoesters <46791177+paulkoesters@users.noreply.github.com>
2019-01-17 11:30:28 -0600

    Initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Paul Koesters
+ * Date: 2019/17/01
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Paul Koesters\n");
+
+  return 0;
+}
